f62da9ec917baadfc0f7a628975aa67c4df65d4e
Wayne <54445994+Waynenet@users.noreply.github.com>
2023-11-26 23:15:42 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,8 +9,8 @@ def list_split(items, n):
 def getdata(name):
     gitpage = requests.get("https://github.com/" + name)
     data = gitpage.text
-    datadatereg = re.compile(r'data-date="(.*?)" data-level')
-    datacountreg = re.compile(r'<span class="sr-only">(.*?) contribution')
+    datadatereg = re.compile(r'data-date="(.*?)" id="contribution-day-component"')
+    datacountreg = re.compile(r'<tool-tip .*?class="sr-only position-absolute">(.*?) contribution')
     datadate = datadatereg.findall(data)
     datacount = datacountreg.findall(data)
     datacount = list(map(int, [0 if i == "No" else i for i in datacount]))
